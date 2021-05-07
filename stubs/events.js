4d0fcb7aeca8e80b2bc975a45a6849680bf190b1
Marvin Raiser <marvin_raiser@web.de>
2021-05-07 15:36:26 +0200

    Add more event stubs

--- stubs/events.js
+++ stubs/events.js
@@ -2,7 +2,7 @@
  * Copy from here!
  */
 
-const { MessageReaction, User, Message } = require("discord.js");
+const { MessageReaction, User, Message, DMChannel, GuildChannel, TextChannel, GuildEmoji } = require("discord.js");
 
 module.exports = {
     message,
@@ -10,29 +10,72 @@ module.exports = {
 }
 
 /**
- * Emitted whenever a chennel is created.
- * @param {DMChannel}
+ * Emitted whenever a channel is created.
+ * @param {DMChannel|GuildChannel} channel The channel that was created
  */
-function channelCreate (channel) {
-
-}
-
-
-
-/**
- * message Event Handler
- * @param {Message} message The received message
- */
-function message(message) {
+function channelCreate(channel) {
 
 }
 
 /**
- * voiceStateUpdate Event Handler
- * @param {VoiceState} oldVoiceState the old voice state of the user
- * @param {VoiceState} newVoiceState the new voice state of the user
+ * Emitted whenever a channel is deleted.
+ * @param {DMChannel|GuildChannel} channel 
  */
-function voiceStateUpdate(oldVoiceState, newVoiceState) {
+function channelDelete(channel) {
 
 }
 
+/**
+ * Emitted whenever the pins of a channel are updated. Due to the nature of the WebSocket event, not much information can be provided easily here - you need to manually check the pins yourself.
+ * @param {DMChannel|TextChannel} channel The channel that the pins update occurred in
+ * @param {Date} time The time of the pins update
+ */
+function channelPinsUpdate(channel, time) {
+
+}
+
+/**
+ * Emitted whenever a channel is updated - e.g. name change, topic change, channel type change.
+ * @param {DMChannel|GuildChannel} oldChannel The channel before the update
+ * @param {DMChannel|GuildChannel} newChannel The channel after the update
+ */
+function channelUpdate(oldChannel, newChannel) {
+
+}
+
+/**
+ * Emotted for general debugging information
+ * @param {string} info The debug information
+ */
+function debug(info) {
+
+}
+
+/**
+ * Emitted whenever a custom emoji is created in a guild.
+ * @param {GuildEmoji} emoji The emoji that was created
+ */
+function emojiCreate(emoji) {
+
+}
+
+/**
+ * Emitted whenever a custom emoji is deleted in a guild.
+ * @param {GuildEmoji} emoji The emoji that was deleted
+ */
+function emojiDelete(emoji) {
+
+}
+
+/**
+ * Emitted whenever a custom emoji is updated in a guild.
+ * @param {GuildEmoji} oldEmoji The old emoji
+ * @param {GuildEmoji} newEmoji The new emoji
+ */
+function emojiUpdate(oldEmoji, newEmoji) {
+
+}
+
+function error(params) {
+
+}
