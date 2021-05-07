e9be40bf5fdb47429b7bd56f5830dd55bd026d1e
Philipp Weiss <philippweiss.game@gmail.com>
2021-05-07 14:53:03 +0200

    Added stats functionalities

--- modules/stats/index.js
+++ modules/stats/index.js
@@ -1,43 +1,109 @@
 const { MessageReaction, User, Message } = require("discord.js");
+const fileLib = require("../../lib/file");
+const logger = require("../../lib/logger");
 
 module.exports = function (bot) {
 
-    /**
-     * 
-     * @param {Message} message 
-     */
-    function ping(message) {
-        message.channel.send("Pong!");
-    }
+    const USER_DATA_FILE_NAME = "userstats";
+    const MAX_EVENT_COUNT = 2;
+    let userData = fileLib.loadData(USER_DATA_FILE_NAME);
+    let eventCounter = 0;
+    let userDataTemplate = {
+        lastLogin: new Date(),
+        totalMessages: 0,
+        totalReactionsSend: 0,
+        totalReactionsRecieved: 0,
+        totalMentions: 0,
+        totalVoiceTime: 0,
+        totalScore: 0
+    };
 
     /**
-     * 
-     * @param {Message} message 
+     * message Event Handler
+     * @param {Message} message The received message
      */
     function message(message) {
-        if (message.cleanContent.includes("ping")) {
-            message.react('🏓');
+        createUser(message.author.id);
+        
+        // TotalMessages
+        userData[message.author.id].totalMessages += 1; 
+        logger.log("Stats", "Message received! userData:", userData);
+        // TotalMentions
+        console.log(message.mentions.users);
+        for (const id of Object.keys(message.mentions.users)) {
+            console.log(id);
+            createUser(id);
+            userData[id].totalMentions += 1;
+        }
+
+        saveUserData();
+    }
+
+    /**
+     * messageReactionAdd Handler
+     * @param {Message} message The received message
+     * @param {User} user The user who send it
+     */
+    function messageReactionAdd(messageReaction, user) {
+        createUser(user.id);
+        userData[user.id].totalReactionsSend = userData[user.id].totalReactionsSend + 1;
+        createUser(messageReaction.message.author.id);
+        userData[messageReaction.message.author.id].totalReactionsRecieved += 1
+    }
+
+    /**
+     * voiceStateUpdate Event Handler
+     * @param {VoiceState} oldVoiceState
+     * @param {VoiceState} newVoiceState
+     */
+     function voiceStateUpdate(oldVoiceState, newVoiceState) {
+        createUser(oldVoiceState.member.id);
+        if (newVoiceState.channel) {
+            userData[oldVoiceState.member.id].lastLogin = new Date();
+        } else {
+            userData[oldVoiceState.member.id].totalVoiceTime += (new Date()).getTime() - userData[oldVoiceState.member.id].lastLogin.getTime(); 
+        }
+        saveUserData();
+    }
+
+    /**
+     * create user if not exists
+     * @param {number} id id of user
+     */
+    function createUser(id) {
+        if(userData[id] == null || userData[id] === undefined){
+            userData[id] = {...userDataTemplate};
         }
     }
 
     /**
-     * 
-     * @param {MessageReaction} messageReaction 
-     * @param {User} user 
+     * Saves userData to the data file everytime when the max event count was reached
      */
-    function messageReactionAdd(messageReaction, user) {
-        if (messageReaction.emoji === "🏓") {
-            messageReaction.message.react("✋");
+    function saveUserData() {
+        if(eventCounter === MAX_EVENT_COUNT - 1) {
+            fileLib.storeData(USER_DATA_FILE_NAME, userData);
+            logger.log("Stats", "Saved UserData!");
         }
+        eventCounter = (eventCounter + 1) % MAX_EVENT_COUNT;
     }
 
+    /**
+     * Merges the read userData with the template once on startup to add potential new properties
+     */
+    function migrate() {
+        for (const userId of Object.keys(userData)) {
+            userData[userId] = {...userDataTemplate, ...userData[userId]};
+        }
+    }
+    migrate();
+
     return {
-        name: "Ping",
+        name: "Stats",
         commands: {
-            ping
         },
         events: {
             message,
+            voiceStateUpdate,
             messageReactionAdd
         }
     }
---
+++ stubs/events.js
@@ -0,0 +1,38 @@
+/**
+ * Copy from here!
+ */
+
+const { MessageReaction, User, Message } = require("discord.js");
+
+module.exports = {
+    message,
+    voiceStateUpdate
+}
+
+/**
+ * Emitted whenever a chennel is created.
+ * @param {DMChannel}
+ */
+function channelCreate (channel) {
+
+}
+
+
+
+/**
+ * message Event Handler
+ * @param {Message} message The received message
+ */
+function message(message) {
+
+}
+
+/**
+ * voiceStateUpdate Event Handler
+ * @param {VoiceState} oldVoiceState the old voice state of the user
+ * @param {VoiceState} newVoiceState the new voice state of the user
+ */
+function voiceStateUpdate(oldVoiceState, newVoiceState) {
+
+}
+
